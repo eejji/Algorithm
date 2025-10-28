c8cd6670ed8ad10fec57d5e51d0486f5b6ecef46
Jihun Lee <jihun78167816@gmail.com>
2025-10-28 21:01:26 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,9 @@
+def solution(a, b, c):
+    if len({a, b, c}) == 3:  
+        return a + b + c
+    
+    elif a == b == c:  
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    
+    else:  
+        return (a + b + c) * (a**2 + b**2 + c**2)
