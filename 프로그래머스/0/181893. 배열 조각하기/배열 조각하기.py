cb4746413757b5c491fc6f6d475d9d78b30b97cc
Jihun Lee <jihun78167816@gmail.com>
2025-12-25 11:05:27 +0900

    [level 0] Title: 배열 조각하기, Time: 0.50 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -0,0 +1,8 @@
+def solution(arr, query):
+    for i in range(len(query)):
+        if i%2==0:
+            del arr[query[i]+1:]
+        else:
+            del arr[:query[i]]
+            
+    return arr
