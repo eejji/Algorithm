53a01c274ba8972c5b066f1e44598869f333fe07
Jihun Lee <jihun78167816@gmail.com>
2025-12-25 10:42:24 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,13 @@
+def solution(n, slicer, num_list):
+    a, b, c = slicer[0], slicer[1], slicer[2]
+    
+    if  n == 1:
+        answer = num_list[:b+1]
+    if n==2:
+        answer = num_list[a:]
+    if n==3:
+        answer = num_list [a:b+1]
+    if n==4:
+        answer = num_list[a:b+1:c]
+        
+    return answer
