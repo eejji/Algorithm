61f08dc9ae0c9af1a9ab5f60795616e0009667a0
Jihun Lee <jihun78167816@gmail.com>
2026-01-04 18:48:44 +0900

    [level 0] Title: ad 제거하기, Time: 0.33 ms, Memory: 9.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,8 @@
+def solution(strArr):
+    a = []
+    for i in range(len(strArr)):
+        if strArr[i].count("ad"):
+            pass
+        else:
+            a.append(strArr[i])
+    return a
