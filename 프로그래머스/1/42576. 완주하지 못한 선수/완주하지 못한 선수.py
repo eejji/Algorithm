fc233b74252c1412161f6a2c71e62a67132a08a6
Jihun Lee <jihun78167816@gmail.com>
2026-01-30 13:45:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.86 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,15 @@
+def solution(participant, completion):
+    hash_dict = {}
+
+    for name in participant:
+        if name in hash_dict:
+            hash_dict[name] += 1
+        else:
+            hash_dict[name] = 1
+
+    for name in completion:
+        hash_dict[name] -= 1
+
+    for name in hash_dict:
+        if hash_dict[name] == 1:
+            return name
