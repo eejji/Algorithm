e9513ac17fe38b0e3121723763daf69faf52a21b
Jihun Lee <jihun78167816@gmail.com>
2026-02-11 10:53:44 +0900

    [Bronze III] Title: 직각삼각형, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,13 @@
+while True:
+    li = list(map(int, input().split()))
+
+    if sum(li) == 0:
+        break
+
+    max_num = max(li)
+    li.remove(max_num)
+
+    if max_num**2 == li[0]**2 + li[1]**2:
+        print('right')
+    else:
+        print('wrong')
