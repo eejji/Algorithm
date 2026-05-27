f5e5b33f04175e1aae0561281d04aeefc4ca023b
Jihun Lee <jihun78167816@gmail.com>
2026-05-27 17:31:46 +0900

    [level 2] Title: 피보나치 수, Time: 12.76 ms, Memory: 15.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,15 @@
+def solution(n):
+    dp = [0] * (n+1)
+
+    dp[0] = 0
+    dp[1] = 1
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        for i in range(2, n+1):
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+
+        return dp[n]
